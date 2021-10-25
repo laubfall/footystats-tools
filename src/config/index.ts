5839733173840a271e3c274cb9ece9a27a95fee6
daniel <d.ludwig@micromata.de>
2021-10-25 19:55:23 +0200

    Configuration type

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,10 @@
 const env = process.env.NODE_ENV || 'development';
+
 // eslint-disable-next-line import/no-dynamic-require
 const cfg = require(`./config.${env}.js`);
-module.exports = cfg;
+
+export interface Config {
+  db: string;
+}
+
+export default cfg as Config;
