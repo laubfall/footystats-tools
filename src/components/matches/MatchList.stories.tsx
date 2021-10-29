e9e2c140ded1ff43e81c0ad695c95cbe2564a0f9
daniel <d.ludwig@micromata.de>
2021-10-29 19:29:47 +0200

    Added storybook

---
+++ src/components/matches/MatchList.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { MatchList } from './MatchList';
+
+export default {
+  title: 'Components/matches',
+};
+
+export const Primary = () => <MatchList />;
---
+++ src/components/matches/MatchList.tsx
@@ -0,0 +1,4 @@
+import React, { useEffect } from 'react';
+
+// eslint-disable-next-line import/prefer-default-export
+export const MatchList = () => <h4>huhu</h4>;
