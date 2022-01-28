0207708542ba71db747e4b06f4772bd2874f723d
daniel <d.ludwig@micromata.de>
2022-01-28 13:55:47 +0100

    Fixed path

--- src/__tests__/TestUtils.ts
+++ src/__tests__/TestUtils.ts
@@ -3,7 +3,7 @@ import LeagueStatsService from '../app/services/stats/LeagueStatsService';
 import { MatchStatsService } from '../app/services/stats/MatchStatsService';
 import Configuration from '../app/types/application/Configuration';
 
-jest.mock('../app/services/application/Ipc2RendererService');
+jest.mock('../app/services/application/gui/IpcMain2Renderer');
 
 inversifyContainer.bind(Configuration).toConstantValue(new Configuration());
 const mss = inversifyContainer.get<MatchStatsService>(MatchStatsService);
