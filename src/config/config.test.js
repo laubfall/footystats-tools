fe605e7a9aae9ffdbc0aad3e3549cacf47510d22
daniel <d.ludwig@micromata.de>
2022-04-10 16:31:38 +0200

    Base config

--- src/config/config.test.js
+++ src/config/config.test.js
@@ -1,9 +1,7 @@
+const cfgBase = require('./config.js');
+
 // eslint-disable-next-line no-multi-assign
-const config = (module.exports = {});
-config.matchStatsDbFileName = 'matchStats.nedb';
-config.teamStatsDbFileName = 'teamStats.nedb';
-config.leagueStatsDbFileName = 'leagueStats.nedb';
-config.window = { width: 1600, height: 1200 };
+const config = (module.exports = { ...cfgBase });
 config.markCsvFilesAsImported = false;
 config.nedb = {
   inMemoryOnly: true,
