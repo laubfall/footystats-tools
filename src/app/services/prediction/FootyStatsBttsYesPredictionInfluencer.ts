dcff1d8a45a07eb1518a605184c5ad3e703d1f45
daniel <d.ludwig@micromata.de>
2022-04-16 21:23:01 +0200

    Added new influencer

---
+++ src/app/services/prediction/FootyStatsBttsYesPredictionInfluencer.ts
@@ -0,0 +1,32 @@
+import {
+  Bet,
+  BetPredictionContext,
+} from '../../types/prediction/BetPredictionContext';
+import {
+  BetResultInfluencer,
+  BetInfluencerCalculation,
+  PreCheckReturn,
+  PrecheckResult,
+} from '../../types/prediction/BetResultInfluencer';
+
+class FootyStatsBttsYesPredictionInfluencer implements BetResultInfluencer {
+  // eslint-disable-next-line class-methods-use-this
+  preCheck(ctx: BetPredictionContext): PreCheckReturn {
+    if (ctx.bet !== Bet.BTTS_YES) {
+      return PrecheckResult.DONT_KNOW_WHAT_TO_CALCULATE_FOR_BET;
+    }
+
+    if (!ctx.match['BTTS Average']) {
+      return PrecheckResult.NOT_ENOUGH_INFORMATION;
+    }
+
+    return PrecheckResult.OK;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  calculateInfluence(ctx: BetPredictionContext): BetInfluencerCalculation {
+    return { amount: ctx.match['BTTS Average'] };
+  }
+}
+
+export default FootyStatsBttsYesPredictionInfluencer;
--- src/app/services/prediction/PredictionService.ts
+++ src/app/services/prediction/PredictionService.ts
@@ -3,6 +3,8 @@ import {
   BetResultInfluencer,
   PrecheckResult,
 } from '../../types/prediction/BetResultInfluencer';
+import FootyStatsBttsYesPredictionInfluencer from './FootyStatsBttsYesPredictionInfluencer';
+import FootyStatsOverFTPredictionInfluencer from './FootyStatsOverFTPredictionInfluencer';
 import LeaguePositionDiffInfluencer from './LeaguePositionDiffInfluencer';
 import LeaguePositionInfluencer from './LeaguePositionInfluencer';
 import OddsBttsYesInfluencer from './OddsBttsYesInfluencer';
@@ -15,21 +17,36 @@ const betResultInfluencer: BetResultInfluencer[] = [
   new LeaguePositionInfluencer(),
   new LeaguePositionDiffInfluencer(),
   new OddsGoalsOverInfluencer(),
+  new FootyStatsOverFTPredictionInfluencer(),
   new OddsBttsYesInfluencer(),
+  new FootyStatsBttsYesPredictionInfluencer(),
 ];
 
 export default function prediction(ctx: BetPredictionContext): number {
   let result = 0;
 
+  let doneInfluencerCalculations = 0;
   betResultInfluencer.forEach((influencer) => {
     const preCheckResult = influencer.preCheck(ctx);
     if (preCheckResult === PrecheckResult.OK) {
       const predictionInfluence = influencer.calculateInfluence(ctx);
       if (!predictionInfluence.notExecutedCause) {
         result += predictionInfluence.amount;
+        doneInfluencerCalculations += 1;
       }
     }
   });
 
-  return Math.round(result);
+  return Math.round(result / doneInfluencerCalculations);
+}
+
+export type PredictionResult = {
+  betSuccessInPercent: number;
+  betOnThis: boolean;
+};
+
+export enum PredictionSuccess {
+  SUCCESS,
+  CLOSE,
+  FAILED,
 }
