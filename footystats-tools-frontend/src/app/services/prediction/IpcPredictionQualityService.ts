d4080199dd4ba1ecbe673d7518d9471b1fd84498
Daniel Ludwig <d.ludwig@micromata.de>
2022-10-26 14:14:34 +0200

    Removed electron linter stuff.

--- footystats-tools-frontend/src/app/services/prediction/IpcPredictionQualityService.ts
+++ footystats-tools-frontend/src/app/services/prediction/IpcPredictionQualityService.ts
@@ -1,29 +1,26 @@
-import {
-  Precast,
-  PredictionQualityReport,
-  PredictionQualityRevision,
-} from "./PredictionQualityService.types";
+import {Precast, PredictionQualityReport, PredictionQualityRevision,} from "./PredictionQualityService.types";
 
 class IpcPredictionQualityService {
-  computeQuality(): Promise<PredictionQualityReport> {
-    return Promise.resolve(undefined);
-  }
+	computeQuality(): Promise<PredictionQualityReport> {
 
-  latestRevision(): Promise<PredictionQualityRevision> {
-    return Promise.resolve(undefined);
-  }
+		return Promise.resolve(undefined);
+	}
 
-  precast(revision?: PredictionQualityRevision): Promise<Precast> {
-    return Promise.resolve(undefined);
-  }
+	latestRevision(): Promise<PredictionQualityRevision> {
+		return Promise.resolve(undefined);
+	}
 
-  latestReport(): Promise<PredictionQualityReport> {
-    return Promise.resolve(undefined);
-  }
+	precast(revision?: PredictionQualityRevision): Promise<Precast> {
+		return Promise.resolve(undefined);
+	}
 
-  recomputeQuality(revision: number): Promise<PredictionQualityReport> {
-    return Promise.resolve(undefined);
-  }
+	latestReport(): Promise<PredictionQualityReport> {
+		return Promise.resolve(undefined);
+	}
+
+	recomputeQuality(revision: number): Promise<PredictionQualityReport> {
+		return Promise.resolve(undefined);
+	}
 }
 
 export default IpcPredictionQualityService;
