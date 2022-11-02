371c77070b85bfd4fded0cb4b092f088e44d7f54
Daniel Ludwig <d.ludwig@micromata.de>
2022-11-02 17:43:52 +0100

    Removed old stuff

--- footystats-tools-frontend/src/app/types/General.ts
+++ footystats-tools-frontend/src/app/types/General.ts
@@ -1,20 +1,3 @@
 export type NString = string | null;
 export type NDate = Date | null;
 export type NNumber = number | null;
-
-export type CursorModificationType = "sort" | "skip" | "limit";
-
-export type SortOrder = 1 | -1;
-
-export type SortParameter = {
-	[key: string]: SortOrder;
-};
-
-export type CursorModification = {
-	modification: CursorModificationType;
-	parameter: number | SortParameter;
-};
-
-export type PagedResult<JSON> = [number, Array<JSON>];
-
-export type Result<JSON> = Array<JSON> | PagedResult<JSON>;
