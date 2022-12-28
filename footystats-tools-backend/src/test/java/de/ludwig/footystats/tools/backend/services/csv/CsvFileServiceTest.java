66e4c6114636f78e1b113ef4896d2b547212d7c7
Daniel Ludwig <d.ludwig@micromata.de>
2022-12-28 16:51:26 +0100

    Make use of profile test

--- footystats-tools-backend/src/test/java/de/ludwig/footystats/tools/backend/services/csv/CsvFileServiceTest.java
+++ footystats-tools-backend/src/test/java/de/ludwig/footystats/tools/backend/services/csv/CsvFileServiceTest.java
@@ -7,12 +7,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.io.IOException;
 
 @SpringBootTest
 @ContextConfiguration(classes = {Configuration.class})
+@ActiveProfiles("test")
 public class CsvFileServiceTest {
 
     @Autowired
