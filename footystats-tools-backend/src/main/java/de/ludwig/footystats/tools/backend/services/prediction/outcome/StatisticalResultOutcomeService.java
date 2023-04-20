732a5eb884de91954e7266fe36ca8c85427fda4f
laubfall <ludwig.daniel@gmail.com>
2023-04-20 15:50:06 +0200

    Added caching.

--- footystats-tools-backend/src/main/java/de/ludwig/footystats/tools/backend/services/prediction/outcome/StatisticalResultOutcomeService.java
+++ footystats-tools-backend/src/main/java/de/ludwig/footystats/tools/backend/services/prediction/outcome/StatisticalResultOutcomeService.java
@@ -9,20 +9,21 @@ import de.ludwig.footystats.tools.backend.services.prediction.quality.BetPredict
 import de.ludwig.footystats.tools.backend.services.prediction.quality.view.BetPredictionQualityInfluencerAggregate;
 import de.ludwig.footystats.tools.backend.services.prediction.quality.view.PredictionQualityViewService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Slf4j
 @Service
+@Scope("prototype") // Service holds a cache that depends on data that can change during runtime. So create it every time the service is injected.
 public class StatisticalResultOutcomeService {
 
 	private final BetPredictionQualityRepository betPredictionAggregateRepository;
 
 	private final PredictionQualityViewService predictionQualityViewService;
+
+	private Map<InfluencerPredictionCacheKey, Map<String, List<BetPredictionQualityInfluencerAggregate>>> influencerPredictionCache = new HashMap<>();
 
 	public StatisticalResultOutcomeService(BetPredictionQualityRepository betPredictionAggregateRepository, PredictionQualityViewService predictionQualityViewService) {
 		this.betPredictionAggregateRepository = betPredictionAggregateRepository;
@@ -30,29 +31,39 @@ public class StatisticalResultOutcomeService {
 	}
 
 	public StatisticalResultOutcome compute(final PredictionResult result, Bet bet) {
-
 		final Double matchStatisticalOutcome = calcMatchPredictionStatisticalOutcome(result, bet);
 		if (matchStatisticalOutcome == null) return null;
 
 		return new StatisticalResultOutcome(bet, matchStatisticalOutcome, calcInfluencerStatisticalOutcome(result, bet));
 	}
 
-	private List<InfluencerStatisticalResultOutcome> calcInfluencerStatisticalOutcome(final PredictionResult result, final Bet bet){
-		// TODO Refactor this so the this call is not made anytime this method is executed.
-		Map<String, List<BetPredictionQualityInfluencerAggregate>> influencerResults = predictionQualityViewService.influencerPredictionsAggregated(bet, result.betOnThis());
+	private List<InfluencerStatisticalResultOutcome> calcInfluencerStatisticalOutcome(final PredictionResult result, final Bet bet) {
 		List<InfluencerStatisticalResultOutcome> influencerOutcomes = new ArrayList<>();
 		for (InfluencerResult influencerResult : result.influencerDetailedResult()) {
-			final List<BetPredictionQualityInfluencerAggregate> aggregates = influencerResults.get(influencerResult.influencerName());
+			final List<BetPredictionQualityInfluencerAggregate> aggregates = influencerDistributionByCache(bet, result, influencerResult.influencerName());
 			Optional<BetPredictionQualityInfluencerAggregate> measured = aggregates.stream().filter(a -> a.predictionPercent().equals(influencerResult.influencerPredictionValue())).findFirst();
-			if(measured.isPresent()){
+			if (measured.isPresent()) {
 				BetPredictionQualityInfluencerAggregate influencerAggregate = measured.get();
 				var succeeded = influencerAggregate.betSucceeded().doubleValue();
 				var failed = influencerAggregate.betFailed().doubleValue();
-				influencerOutcomes.add(new InfluencerStatisticalResultOutcome(influencerResult.influencerName(), succeeded / (succeeded+failed)));
+				influencerOutcomes.add(new InfluencerStatisticalResultOutcome(influencerResult.influencerName(), succeeded / (succeeded + failed)));
 			}
 		}
 
 		return influencerOutcomes;
+	}
+
+	private List<BetPredictionQualityInfluencerAggregate> influencerDistributionByCache(Bet bet, PredictionResult result, String influencerName) {
+		var cacheKey = new InfluencerPredictionCacheKey(bet, result.betOnThis());
+		Map<String, List<BetPredictionQualityInfluencerAggregate>> matching;
+		if (influencerPredictionCache.containsKey(cacheKey)) {
+			matching = influencerPredictionCache.get(cacheKey);
+		} else {
+			matching = predictionQualityViewService.influencerPredictionsAggregated(bet, result.betOnThis());
+			influencerPredictionCache.put(cacheKey, matching);
+		}
+
+		return matching.get(influencerName);
 	}
 
 	private Double calcMatchPredictionStatisticalOutcome(PredictionResult result, Bet bet) {
@@ -73,5 +84,8 @@ public class StatisticalResultOutcomeService {
 
 	private double calcStatisticalMatchOutcome(Long success, Long failed) {
 		return (double) success / (double) (success + failed);
+	}
+
+	record InfluencerPredictionCacheKey(Bet bet, boolean betOnThis) {
 	}
 }
