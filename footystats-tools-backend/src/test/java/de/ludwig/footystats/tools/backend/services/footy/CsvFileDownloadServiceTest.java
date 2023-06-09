86634abc5010225efd0a39132e724116b36c8d3f
laubfall <ludwig.daniel@gmail.com>
2023-06-09 17:27:18 +0200

    More Tests for configured csv downloads.

--- footystats-tools-backend/src/test/java/de/ludwig/footystats/tools/backend/services/footy/CsvFileDownloadServiceTest.java
+++ footystats-tools-backend/src/test/java/de/ludwig/footystats/tools/backend/services/footy/CsvFileDownloadServiceTest.java
@@ -11,6 +11,7 @@ import de.ludwig.footystats.tools.backend.services.footy.dls.DownloadCountryLeag
 import java.io.IOException;
 import java.time.LocalDate;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -34,6 +35,12 @@ class CsvFileDownloadServiceTest {
 	@Autowired
 	private DownloadConfigRepository downloadConfigRepository;
 
+	@BeforeEach
+	void setup(){
+		reset(csvHttpClient);
+		downloadConfigRepository.deleteAll();
+	}
+
 	@Test
 	void login() {
 		fileDownloadService.downloadMatchStatsCsvFile(LocalDate.now());
@@ -41,8 +48,9 @@ class CsvFileDownloadServiceTest {
 
 	@Test
 	void download_configured_csv_and_mark_as_dld() throws IOException {
-
-		downloadConfigRepository.insert(DownloadCountryLeagueStatsConfig.builder().footyStatsDlId(4711).downloadBitmask(31).country("Germany").season("2023").build());
+		var currentYear = LocalDate.now().getYear();
+		downloadConfigRepository.insert(DownloadCountryLeagueStatsConfig.builder().footyStatsDlId(4711).downloadBitmask(31).country("Germany").season(
+			String.valueOf(currentYear)).build());
 
 		fileDownloadService.downloadConfiguredStats();
 		// five calls to footystats.org cause we have five different csv files.
@@ -56,6 +64,30 @@ class CsvFileDownloadServiceTest {
 		fileDownloadService.downloadConfiguredStats();
 		reset(csvHttpClient);
 		// No download cause last download is not more than 30 days ago.
+		verify(csvHttpClient, times(0)).connectToFootystatsAndRetrieveFileContent(any(), any());
+
+		// Actually not implemented yet
+		//Assertions.assertNotNull(downloadCountryLeagueStatsConfig.getLastPlayerDownload());
+		//Assertions.assertNotNull(downloadCountryLeagueStatsConfig.getLastMatchDownload());
+	}
+
+	@Test
+	void download_configured_prev_season_csv_and_mark_as_dld() throws IOException {
+		downloadConfigRepository.insert(DownloadCountryLeagueStatsConfig.builder().footyStatsDlId(4711).downloadBitmask(31).country("Germany").season(
+			"2022").build());
+
+		fileDownloadService.downloadConfiguredStats();
+		// five calls to footystats.org cause we have five different csv files.
+		verify(csvHttpClient, times(5)).connectToFootystatsAndRetrieveFileContent(any(), any());
+
+		DownloadCountryLeagueStatsConfig downloadCountryLeagueStatsConfig = downloadConfigRepository.findAll().get(0);
+		Assertions.assertNotNull(downloadCountryLeagueStatsConfig.getLastTeamsDownload());
+		Assertions.assertNotNull(downloadCountryLeagueStatsConfig.getLastTeams2Download());
+		Assertions.assertNotNull(downloadCountryLeagueStatsConfig.getLastLeagueDownload());
+
+		fileDownloadService.downloadConfiguredStats();
+		reset(csvHttpClient);
+		// no download cause csv files of old seasons are only downloaded once
 		verify(csvHttpClient, times(0)).connectToFootystatsAndRetrieveFileContent(any(), any());
 
 		// Actually not implemented yet
