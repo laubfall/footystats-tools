a6ac7d042ed4723446b5dc2c60c12c1870571306
laubfall <ludwig.daniel@gmail.com>
2023-08-15 15:40:12 +0200

    Cancel build in case of errors.

--- scripts/buildAll.sh
+++ scripts/buildAll.sh
@@ -1,17 +1,46 @@
 #!/bin/bash
 
+# Definieren Sie die Funktion check_mvn_exit_status
+check_exit_status() {
+    # Übernehmen Sie den Namen des Kommandos als ersten Parameter
+    local command_name="$1"
+
+    # Überprüfen Sie den Exit-Status des Kommandos
+    if [ $? -ne 0 ]; then
+        # Beenden Sie das Skript, wenn das Kommando fehlgeschlagen ist
+        echo "Das $command_name-Kommando ist fehlgeschlagen. Das Skript wird beendet."
+        exit 1
+    fi
+}
+
+# Definieren Sie die Funktion check_mvn_output
+check_mvn_output() {
+    # Übernehmen Sie das Ergebnis des mvn-Kommandos als ersten Parameter
+    local mvn_output="$1"
+
+    # Überprüfen Sie, ob das Ergebnis des mvn-Kommandos einen Fehler enthält
+    if [[ $mvn_output == *"BUILD FAILURE"* ]]; then
+        # Beenden Sie das Skript, wenn das mvn-Kommando fehlgeschlagen ist
+        echo "Das mvn-Kommando ist fehlgeschlagen. Das Skript wird beendet."
+        exit 1
+    fi
+}
+
 finalJarName=footystats-tools-backend.jar
 
 cd ../footystats-tools-backend || exit
 
 echo "Bump backend version"
-mvn versions:set -DnewVersion=$1 -DgenerateBackupPoms=false
+check_mvn_output "$(mvn versions:set -DnewVersion=$1 -DgenerateBackupPoms=false)"
+check_exit_status
 
 echo "Create frontend artifacts that match prod requirements (api url)"
-mvn -DskipTests -Dfrontend.api.generator.backendurl=footystats-tools -P openapi install
+check_mvn_output "$(mvn -DskipTests -Dfrontend.api.generator.backendurl=footystats-tools -P openapi install)"
+check_exit_status
 
 echo "Build footystats-tools backend"
-mvn package
+check_mvn_output "$(mvn package)"
+check_exit_status
 
 cd - || exit
 echo "Build footystats-tools frontend"
@@ -19,8 +48,10 @@ cd ../footystats-tools-frontend || exit
 
 echo "Bump frontend version"
 npm version --allow-same-version $1
+check_exit_status
 rm -rf dist
 npm run-script build
+check_exit_status
 
 cd ..
 zipName=footystats-release-$1.zip
