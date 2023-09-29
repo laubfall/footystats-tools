1fd2c10fa7a83569e2113701e84aa2b4e89ef58f
laubfall <ludwig.daniel@gmail.com>
2023-09-29 10:44:31 +0200

    Simplified mongodb configuration (removed extra class for converter configuration).

--- footystats-tools-backend/src/main/java/de/footystats/tools/mongo/MongoConfiguration.java
+++ footystats-tools-backend/src/main/java/de/footystats/tools/mongo/MongoConfiguration.java
@@ -11,8 +11,15 @@ import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCu
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.Jsr310Converters;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @Configuration
 public class MongoConfiguration {
@@ -30,5 +37,21 @@ public class MongoConfiguration {
 		CodecRegistry codecRegistry = CodecRegistries.fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
 			CodecRegistries.fromCodecs(codecs));
 		return settings -> settings.codecRegistry(codecRegistry);
+	}
+
+	@Bean
+	public MappingMongoConverter mongoConverter(MongoDatabaseFactory dbFactory, MongoMappingContext mongoMappingContext, ApplicationContext context) {
+		DbRefResolver dbRefResolver = new DefaultDbRefResolver(dbFactory);
+		MappingMongoConverter mongoConverter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+		mongoConverter.setCustomConversions(customConversions(context));
+		return mongoConverter;
+	}
+
+	private MongoCustomConversions customConversions(ApplicationContext context) {
+		List<Object> converters = new ArrayList<>();
+		converters.addAll(Jsr310Converters.getConvertersToRegister());
+		Map<String, Converter> beansOfType = context.getBeansOfType(Converter.class);
+		converters.addAll(beansOfType.values());
+		return new MongoCustomConversions(converters);
 	}
 }
