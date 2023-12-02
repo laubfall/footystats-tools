6c13e22489215d2978a306e54c800c2ff87b980c
laubfall <ludwig.daniel@gmail.com>
2023-12-02 13:54:59 +0100

    Check the influencer rankings and use them in case the overall ranking did not match the 10/20 percent target.

--- footystats-tools-frontend/src/components/matches/BetPredictionIcon.tsx
+++ footystats-tools-frontend/src/components/matches/BetPredictionIcon.tsx
@@ -16,19 +16,49 @@ export const BetPredictionIcon = (props: BetPredictionIconProps) => {
 	const prediction = props.predictionResult;
 	const statisticalOutcome = props.statisticalResultOutcome;
 
+	let b10 = statisticalOutcome.ranking?.best10Percent;
+	if (!b10) {
+		// find a influencer with a best10Percent
+		const influencers =
+			statisticalOutcome.influencerStatisticalResultOutcomes;
+		if (influencers) {
+			for (const element of influencers) {
+				if (element.ranking?.best10Percent) {
+					b10 = true;
+					break;
+				}
+			}
+		}
+	}
+
+	let b20 = statisticalOutcome.ranking?.best20Percent;
+	if (!b20) {
+		// find a influencer with a best10Percent
+		const influencers =
+			statisticalOutcome.influencerStatisticalResultOutcomes;
+		if (influencers) {
+			for (const element of influencers) {
+				if (element.ranking?.best20Percent) {
+					b20 = true;
+					break;
+				}
+			}
+		}
+	}
+
 	let BetSuggestionIcon: React.JSX.Element;
 	if (prediction.betOnThis) {
-		if (statisticalOutcome?.ranking?.best10Percent) {
+		if (b10) {
 			BetSuggestionIcon = <BsAirplaneFill />;
-		} else if (statisticalOutcome?.ranking?.best20Percent) {
+		} else if (b20) {
 			BetSuggestionIcon = <BsFillArrowUpCircleFill />;
 		} else {
 			BetSuggestionIcon = <BsFillArrowUpLeftCircleFill />;
 		}
 	} else if (!prediction.betOnThis) {
-		if (statisticalOutcome?.ranking?.best10Percent) {
+		if (b10) {
 			BetSuggestionIcon = <BsAirplane />;
-		} else if (statisticalOutcome?.ranking?.best20Percent) {
+		} else if (b20) {
 			BetSuggestionIcon = <BsArrowUpLeftCircle />;
 		} else {
 			BetSuggestionIcon = <BsFillArrowDownCircleFill />;
