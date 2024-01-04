e963c6d468e377d8f12c7788f5f7afc57eb3345e
laubfall <ludwig.daniel@gmail.com>
2024-01-04 11:55:15 +0100

    Don't use the cookie expire time directly for validation footystats session.

--- footystats-tools-backend/src/main/java/de/footystats/tools/services/footy/SessionCookie.java
+++ footystats-tools-backend/src/main/java/de/footystats/tools/services/footy/SessionCookie.java
@@ -4,18 +4,23 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import org.apache.commons.lang3.StringUtils;
 
+/**
+ * Represents a session cookie as returned by the footystats.org website.
+ */
 public record SessionCookie(String cookie) {
 
-	public LocalDateTime validUntil() {
+	public boolean cookieIsValid() {
+		// The cookie lifetime is not the session lifetime. Actually the session lifetime of
+		// footystats.org is unknown, so add 6 hours to the current point in time to be sure.
+		return validUntil().isAfter(LocalDateTime.now().plusHours(6));
+	}
+
+	LocalDateTime validUntil() {
 		// Optional[PHPSESSID=eb47fq8lde9qijvgmmv3j0ktr3; expires=Thu, 26-Oct-2023 05:16:51 GMT; Max-Age=129600; path=/]
 
 		String expiresRaw = StringUtils.substringBetween(cookie, "expires=", ";");
 		expiresRaw = StringUtils.replace(expiresRaw, "-", " ");
 
 		return LocalDateTime.parse(expiresRaw, DateTimeFormatter.RFC_1123_DATE_TIME);
-	}
-
-	public boolean cookieIsValid() {
-		return validUntil().isAfter(LocalDateTime.now());
 	}
 }
